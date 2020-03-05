c4d6363c9a622b31d1fd0b3f8f8973ebc9549f4c
unknown <wstarvall@gmail.com>
2020-03-05 19:38:10 +0100

    Added some new functions

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var helloWorld = "Hello world"
 
+
+var testArray1 = ["a", "b", "c", "d"];
+var testArray2 = [10, 11, 12, 13, "b"];
+
 printShit(helloWorld);
+testSomeArrayStuff(testArray1);
+dunnoYet();
 
 function printShit(text){
     console.log(text);
+}
+
+function testSomeArrayStuff(array){
+    var i = 0;
+    array.forEach(element => {
+        console.log(element + i);
+        i++;
+    });
+}
+
+function dunnoYet(){
+    while(true) {
+        console.log(Date.UTC);
+    }
+    JSON.
 }
